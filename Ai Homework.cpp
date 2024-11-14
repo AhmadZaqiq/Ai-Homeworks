90295d44e44565f3a22a0554df78e37252d3bb1b
Ahmad Zaqiq <160167174+AhmadZaqiq@users.noreply.github.com>
2024-11-14 20:55:50 +0200

    Update Ai Homework.cpp
    Using a vector instead of a static array

--- Ai Homework.cpp
+++ Ai Homework.cpp
@@ -1,126 +1,122 @@
-//Ahmad JR
 #include <iostream>
 #include <cmath>
+#include <vector>
 using namespace std;
 
 void SetGameState(int State[11][20])
 {
-	int Temp[11][20] =
-	{
-	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-	{1,0,3,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1},
-	{1,0,1,1,0,1,0,1,1,1,1,1,1,0,1,0,1,1,0,1},
-	{1,0,1,2,0,0,0,2,2,2,2,2,2,0,2,0,0,1,0,1},
-	{1,0,1,2,1,1,2,1,1,3,0,1,1,0,1,1,0,1,0,1},
-	{1,0,2,2,2,2,2,1,0,0,0,0,1,0,0,0,0,0,0,1},
-	{1,0,1,2,1,1,2,1,1,1,1,1,1,2,1,1,0,1,2,1},
-	{1,0,1,2,0,4,2,2,2,2,2,2,2,0,0,0,0,1,2,1},
-	{1,0,1,1,0,1,2,1,1,1,1,1,1,0,1,2,1,1,2,1},
-	{1,0,0,0,0,1,2,2,2,0,0,0,0,0,1,2,2,2,2,1},
-	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
-	};
+    int Temp[11][20] =
+    {
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+        {1,0,3,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1},
+        {1,0,1,1,0,1,0,1,1,1,1,1,1,0,1,0,1,1,0,1},
+        {1,0,1,2,0,0,0,2,2,2,2,2,2,0,2,0,0,1,0,1},
+        {1,0,1,2,1,1,2,1,1,3,0,1,1,0,1,1,0,1,0,1},
+        {1,0,2,2,2,2,2,1,0,0,0,0,1,0,0,0,0,0,0,1},
+        {1,0,1,2,1,1,2,1,1,1,1,1,1,2,1,1,0,1,2,1},
+        {1,0,1,2,0,4,2,2,2,2,2,2,2,0,0,0,0,1,2,1},
+        {1,0,1,1,0,1,2,1,1,1,1,1,1,0,1,2,1,1,2,1},
+        {1,0,0,0,0,1,2,2,2,0,0,0,0,0,1,2,2,2,2,1},
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+    };
 
-	for (int i = 0; i < 11; i++)
-	{
-		for (int j = 0; j < 20; j++)
-		{
-			State[i][j] = Temp[i][j];
-		}
-	}
-
+    for (int i = 0; i < 11; i++)
+    {
+        for (int j = 0; j < 20; j++)
+        {
+            State[i][j] = Temp[i][j];
+        }
+    }
 }
 
 void PrintState(int State[11][20])
 {
-	for (int i = 0; i < 11; i++)
-	{
-		for (int j = 0; j < 20; j++)
-		{
-			cout << State[i][j] << "  ";
-		}
-		cout << "\n\n";
-	}
-
+    for (int i = 0; i < 11; i++)
+    {
+        for (int j = 0; j < 20; j++)
+        {
+            cout << State[i][j] << "  ";
+        }
+        cout << "\n\n";
+    }
 }
 
 int PackmanHeuristicPills(int State[11][20])
 {
-	int PillsSum = 0;
+    int PillsSum = 0;
 
-	for (int i = 0; i < 11; i++)
-	{
-		for (int j = 0; j < 20; j++)
-		{
-			if (State[i][j] == 2)
-			{
-				PillsSum++;
-			};
-		}
-	}
+    for (int i = 0; i < 11; i++)
+    {
+        for (int j = 0; j < 20; j++)
+        {
+            if (State[i][j] == 2)
+            {
+                PillsSum++;
+            }
+        }
+    }
 
-	return PillsSum;
+    return PillsSum;
 }
 
 int CalculateDistance(int PackmanX, int PackmanY, int GhostX, int GhostY)
 {
-	return (abs(GhostY - PackmanY) + abs(GhostX - PackmanX));
+    return (abs(GhostY - PackmanY) + abs(GhostX - PackmanX));
 }
 
-int CalculateShortestDistance(int GhostDistances[50], int ArrSize)
+int CalculateShortestDistance(const vector<int>& GhostDistances)
 {
-	int Min = GhostDistances[0];
+    int Min = GhostDistances[0];
 
-	for (int i = 0; i < ArrSize; i++)
-	{
-		if (GhostDistances[i] < Min)
-		{
-			Min = GhostDistances[i];
-		};
-	}
+    for (int distance : GhostDistances)
+    {
+        if (distance < Min)
+        {
+            Min = distance;
+        }
+    }
 
-	return Min;
+    return Min;
 }
 
 int PackmanHeuristicGhostDistance(int State[11][20])
 {
-	int GhostDistances[50];
-	int ArrCounter = 0;
+    vector<int> GhostDistances;
 
-	for (int i = 0; i < 11; i++)
-	{
-		for (int j = 0; j < 20; j++)
-		{
-			if (State[i][j] == 3)
-			{
-				GhostDistances[ArrCounter] = CalculateDistance(5, 7, j, i);
-				ArrCounter++;
-			}
-		}
-	}
+    for (int i = 0; i < 11; i++)
+    {
+        for (int j = 0; j < 20; j++)
+        {
+            if (State[i][j] == 3)
+            {
+                GhostDistances.push_back(CalculateDistance(5, 7, j, i));
+            }
+        }
+    }
 
-	return CalculateShortestDistance(GhostDistances, ArrCounter);
+    return CalculateShortestDistance(GhostDistances);
 }
 
-void DisplayHeuristicsValues(int State[11][20], int Score, int GhostDistance)
+void DisplayHeuristicsValues(int Score, int GhostDistance)
 {
-	cout << "\n\nPills={" << Score << "}\n\n";
-	cout << "\n\The Nearest ghost={" << GhostDistance << "}\n\n";
+    cout << "\n\nPills={" << Score << "}\n\n";
+    cout << "\n\nNearest Ghost={" << GhostDistance << "}\n\n";
 }
 
 int main()
 {
-	int State[11][20];
+    int State[11][20];
 
-	SetGameState(State);
+    SetGameState(State);
 
-	int Score = PackmanHeuristicPills(State);
-	int GhostDistance = PackmanHeuristicGhostDistance(State);
+    int Score = PackmanHeuristicPills(State);
+    int GhostDistance = PackmanHeuristicGhostDistance(State);
 
-	PrintState(State);
+    PrintState(State);
 
-	DisplayHeuristicsValues(State, Score, GhostDistance);
+    DisplayHeuristicsValues(Score, GhostDistance);
 
-	system("pause>0");
+    system("pause>0");
 
-	return 0;
+    return 0;
 }
